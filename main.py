449f4305490a85c713b07f391a83477f48c366b7
P-Odiix <mahingarodin@gmail.com>
2024-11-20 21:09:16 +0200

    "applied linear regression to y_lr_train_pred and y_lr_test_pred"

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/delaney_solubility_with_descriptors.csv')
-df
+print(df)
 
 y = df['logS']
-y
+print(y)
 
 x=df.drop('logS', axis= 1)
-x
+print(x)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
+
+from sklearn.linear_model import LinearRegression
+
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+
+y_lr_train_pred = lr.predict(X_train)
+y_lr_test_pred = lr.predict(X_test)
+print(y_lr_train_pred,y_lr_test_pred)
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+
+lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+print('LR MSE (Train) : ' , lr_train_mse )
+print('LR MSE (Train) : ' , lr_train_r2)
+print('LR MSE (Test) : ' , lr_test_mse)
+print('LR MSE (Test) : ' , lr_test_r2)

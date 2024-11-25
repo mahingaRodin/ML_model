a72bfe38d215dcdbb4cbdcd0571457de1c0434b3
P-Odiix <mahingarodin@gmail.com>
2024-11-25 20:33:32 +0200

    "applied linear regression to y_lr_train_pred and y_lr_test_pred"

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import pandas as pd
-
+# Loading Data from the link
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/delaney_solubility_with_descriptors.csv')
 print(df)
 
+# Data preparation && separation as x and y
 y = df['logS']
 print(y)
-
 x=df.drop('logS', axis= 1)
 print(x)
 
+# Data splitting
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=100)
-
+# Model building
+# Linear regression
 from sklearn.linear_model import LinearRegression
 
 lr = LinearRegression()
+# Training the model
 lr.fit(X_train, y_train)
 
+#Applying the model to make predictions
 y_lr_train_pred = lr.predict(X_train)
 y_lr_test_pred = lr.predict(X_test)
 print(y_lr_train_pred,y_lr_test_pred)
 
+# Evaluating model performance
 from sklearn.metrics import mean_squared_error, r2_score
 
 lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
@@ -32,3 +37,26 @@ print('LR MSE (Train) : ' , lr_train_mse )
 print('LR MSE (Train) : ' , lr_train_r2)
 print('LR MSE (Test) : ' , lr_test_mse)
 print('LR MSE (Test) : ' , lr_test_r2)
+
+lr_results = pd.DataFrame(['Linear regression ', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+lr_results.columns = ['Method' , 'Training MSE' , 'Training R2' , 'Test MSE' , 'Test R2']
+print(lr_results)
+
+# Random forest
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor(max_depth=2, random_state=100)
+rf.fit(X_train, y_train)
+y_rf_train_pred = rf.predict(X_train)
+y_rf_test_pred = rf.predict(X_test)
+
+from sklearn.metrics import mean_squared_error, r2_score
+
+rf_train_mse = mean_squared_error(y_train, y_rf_train_pred)
+rf_train_r2 = r2_score(y_train, y_rf_train_pred)
+
+rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
+rf_test_r2 = r2_score(y_test, y_rf_test_pred)
+
+rf_results = pd.DataFrame(['Random Forest', rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
+rf_results.columns = ['Method' , 'Training MSE' , 'Training R2' , 'Test MSE' , 'Test R2']
+
